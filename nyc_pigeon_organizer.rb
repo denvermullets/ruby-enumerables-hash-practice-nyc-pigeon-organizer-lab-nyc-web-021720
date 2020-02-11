6fbd5b2922a3ab161cadb16d529b3b2e4ff9ec45
Ryan Vaznis <ryan.vaznis@gmail.com>
2020-02-11 21:52:41 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,27 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+
+# %w(foo bar).each_with_object({}) { |str, hsh| hsh[str] = str.upcase }
+# => {'foo' => 'FOO', 'bar' => 'BAR'}
+  
+  #create my new hash that'll be organized the way we want
+  bird_mans = {}
+  
+  # pull the 1st set of keys
+  data.each do |all_sub_cats, value|
+    # pp all_sub_cats
+      # returns => :color :gender :lives
+    # pp value
+      # returns the subhashes => :purple // :male // "locations"
+    
+    # now we need to pull the sub hash keys
+    value.each do |sub_cat_info, value|
+     
+  end 
+  
+  
+  
+  
+  
+  
+  
 end

38a4dd2f2d43ea58666f7ff03821610c5d8220d8
Ryan Vaznis <ryan.vaznis@gmail.com>
2020-02-11 22:08:22 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,5 @@
 def nyc_pigeon_organizer(data)
 
-# %w(foo bar).each_with_object({}) { |str, hsh| hsh[str] = str.upcase }
-# => {'foo' => 'FOO', 'bar' => 'BAR'}
-  
   #create my new hash that'll be organized the way we want
   bird_mans = {}
   

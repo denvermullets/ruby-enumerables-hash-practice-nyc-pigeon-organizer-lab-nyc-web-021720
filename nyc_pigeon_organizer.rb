118788344e227ff83f800cbef31bd368f35d0113
Ryan Vaznis <ryan.vaznis@gmail.com>
2020-02-11 21:56:13 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -10,13 +10,13 @@ def nyc_pigeon_organizer(data)
   data.each do |all_sub_cats, value|
     # pp all_sub_cats
       # returns => :color :gender :lives
-    # pp value
+    pp value
       # returns the subhashes w/arrays => :purple // :male // "locations"
     
     # now we need to pull the sub hash keys
     value.each do |sub_cat_info, names_of_birds|
-      pp sub_cat_info
-        # returns hashes => :
+      # pp sub_cat_info
+        # returns hashes => :purple // :male // "locations"
     end
       
            
